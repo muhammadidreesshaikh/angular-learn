b9eabcf329a56ef0f8c526e0c6ecca6b952225d3
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-10-12 20:33:49 +0500

    For Loop | *ngFor & Nested Loop | Multidimensional Array both of done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,6 +18,24 @@ export class HomeComponent implements OnInit {
   color = "blue";
   switchColors = "abc";
 
+  // Loop
+  users=['Anil', 'basker', 'Adom', 'Ancy'];
+  userDetails=[
+    {name: 'Anil', email: 'anil@email.com', phone: '0215543302'},
+    {name: 'Basker', email: 'basker@email.com', phone: '0215812000'},
+    {name: 'Adom', email: 'adom@email.com', phone: '0211473399'},
+    {name: 'Ancy', email: 'ancy@email.com', phone: '0215111950'},
+  ];
+
+  // Nested-Loop
+  member=[
+    {name: 'Anil', phone: '12345', socialAccounts:['facebook', 'instagram', 'whastapp']},
+    {name: 'Samy', phone: '98765', socialAccounts:['whastapp', 'youtube', 'gmail']},
+    {name: 'Anecy', phone: '23457', socialAccounts:['gmail', 'whastapp', 'yahoo']},
+    {name: 'Jommy', phone: '65432', socialAccounts:['youtube', 'whastapp', 'gmail']},
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
